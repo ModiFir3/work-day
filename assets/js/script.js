ed8b7010ca8bdd25db581ea187544efe19762055
PaulS <psoliz333@gmail.com>
2022-03-12 19:57:22 -0600

    added local storage for input values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-var event = {};
-
 //displaying timezone on the top of the page
 var centralTime = document.querySelector('#texas-time');
 centralTime.textContent = moment().format('dddd, MMMM Do YYYY');
 
+$('.save-btn').on('click', function () {
+    var text = $(this).siblings('.textinput').val();
+    var time = $(this).siblings('.hour').attr('id');
+
+    localStorage.setItem(time, text);
+})
