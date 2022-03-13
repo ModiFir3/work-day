079fd450edf04d7d5037f84bb2d6e29246dcedd6
PaulS <psoliz333@gmail.com>
2022-03-12 19:15:11 -0600

    added main html files for textarea, input, and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var event = {};
+
+//displaying timezone on the top of the page
+var centralTime = document.querySelector('#texas-time');
+centralTime.textContent = moment().format('dddd, MMMM Do YYYY');
+
